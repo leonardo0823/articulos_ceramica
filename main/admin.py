b6f6e08853f7e57fb0cf884fc08aeae810e65b17
leonardo <mrleonardo@gmail.com>
2023-06-29 18:58:33 +0200

    Clean all code

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Articulo, Taller
 
-# Register your models here.
-
 @admin.register(Articulo)
 class ArticuloAdmin(admin.ModelAdmin):
     list_display = ['codigo', 'nombre', 'costo', 'cantidad_producida_en_el_mes', 'taller', 'mes_proceso', 'cantidad_rechazada']
